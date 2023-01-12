1e9a7c3482b5436374230a6f476f25764340066c
Zouv4ik <prostozzouv4ik@gmail.com>
2023-01-12 21:14:14 +0300

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
-import Head from 'next/head'
+import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-     <h1>Hello!</h1>
+     <Image
+      src=".../public/image1.png"
+      width={50}
+      height={50}
+      alt="Test"
+     />
     </div>
   )
 }
