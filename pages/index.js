97a8d1ace80bb83265ebccab28996803595ff79e
Zouv4ik <prostozzouv4ik@gmail.com>
2023-01-12 21:17:14 +0300

    Update

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
      <Image
-      src=".../public/image1.png"
+      src="../public/image1.png"
       width={50}
       height={50}
       alt="Test"
