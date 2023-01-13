1e0f87d48780f8613fa343e7f574c853f138d9b1
NEDOSTUPN0 <v.ya.1910@gmail.com>
2023-01-13 11:10:28 +0300

    Update

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,7 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Ку Влага</h1>
-      <h3 style="font-size: 35px">Hola amigos</h3>
+      <h1>Small шкильник</h1>
     </div>
   )
 }
